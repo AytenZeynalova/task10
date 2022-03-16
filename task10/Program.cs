13cb9c8b76495f0bde27f6716cd31c879c8d8ce0
unknown <14rvxpg@code.edu.az>
2022-03-16 18:00:02 +0400

    created

---
+++ task10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //verilmis ededler siyahisindan cut ededlerden ibaret array deuzelden proqram
+
+            Console.WriteLine("ededlerin sayini daxil edin:");
+            string number = Console.ReadLine();
+            int num = Convert.ToInt32(number);
+            Console.WriteLine("ededleri daxil edin:");
+            
+            int[] numbers = new int[num];
+            int count = 0;
+            
+           
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                if (numbers[i] % 2 == 0)
+                {
+                    count++;
+                }
+                
+            }
+
+            int[] numcut = new int [count];
+            int j = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    numcut[j] = numbers[i];
+                    j++;
+
+                }
+            }
+            Console.WriteLine("cut ededler arrayi:");
+            foreach(var item in numcut)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
